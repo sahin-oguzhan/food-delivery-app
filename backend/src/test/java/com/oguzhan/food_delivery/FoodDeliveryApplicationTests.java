bbd4e4ac931a4a7995d6068dd32a3ac5fffd216b
sahin-oguzhan <oguzhan.sahin99@outlook.com>
2026-05-10 00:46:27 +0300

    Inital project stucture and database configuration

---
+++ backend/src/test/java/com/oguzhan/food_delivery/FoodDeliveryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.oguzhan.food_delivery;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FoodDeliveryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
