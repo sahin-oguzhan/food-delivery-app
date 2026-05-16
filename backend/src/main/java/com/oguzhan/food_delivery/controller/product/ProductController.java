35e5479091fdeacf1d1912cb18181dcf52e541ae
sahin-oguzhan <oguzhan.sahin99@outlook.com>
2026-05-16 19:12:26 +0300

    fix(product): use @ModelAttribute to resolve Swagger multipart file upload issue

--- backend/src/main/java/com/oguzhan/food_delivery/controller/product/ProductController.java
+++ backend/src/main/java/com/oguzhan/food_delivery/controller/product/ProductController.java
@@ -4,16 +4,12 @@ import com.oguzhan.food_delivery.dto.product.ProductRequest;
 import com.oguzhan.food_delivery.dto.product.ProductResponse;
 import com.oguzhan.food_delivery.dto.product.ProductUpdateRequest;
 import com.oguzhan.food_delivery.service.product.ProductService;
-import io.swagger.v3.oas.annotations.Parameter;
-import io.swagger.v3.oas.annotations.media.Schema;
-import io.swagger.v3.oas.annotations.security.SecurityRequirement;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-import org.springframework.web.multipart.MultipartFile;
 
 import java.io.IOException;
 import java.util.List;
